0384ec43ddc22133bba413ecb0577ae5badea423
Damirados <damirados@gmail.com>
2024-10-23 14:19:44 +0200

    Add supervision tree

---
+++ lib/nerves_wfb_ng/application.ex
@@ -0,0 +1,20 @@
+defmodule NervesWfbNg.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: NervesWfbNg.Worker.start_link(arg)
+      # {NervesWfbNg.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: NervesWfbNg.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

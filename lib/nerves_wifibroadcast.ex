61cff3e49912320e4479b7b1946ec144ae765d1d
Damirados <damirados@gmail.com>
2025-09-08 01:31:36 +0200

    Improve wfb wrapper

--- lib/nerves_wifibroadcast.ex
+++ lib/nerves_wifibroadcast.ex
@@ -1,4 +1,4 @@
-defmodule NervesWfbNg do
+defmodule NervesWifibroadcast do
   require Bundlex.Port
 
   @moduledoc """
@@ -9,9 +9,9 @@ defmodule NervesWfbNg do
   Sets card into monitor mode.
   """
   def set_card_monitor_mode(card) do
-    with {:ok, _} <- cmd("ip", ["link", "set", card, "down"]),
-         {:ok, _} <- cmd("iw", ["dev", card, "set", "monitor", "otherbss"]),
-         {:ok, _} <- cmd("ip", ["link", "set", card, "up"]),
+    with {:ok, _} <- mt_cmd("ip", ["link", "set", card, "down"]),
+         {:ok, _} <- mt_cmd("iw", ["dev", card, "set", "monitor", "otherbss"]),
+         {:ok, _} <- mt_cmd("ip", ["link", "set", card, "up"]),
          do: :ok
   end
 
@@ -22,7 +22,7 @@ defmodule NervesWfbNg do
     channel = to_string(channel)
     width = to_string(width)
 
-    with {:ok, _} <- cmd("iw", ["dev", card, "set", channel, width]),
+    with {:ok, _} <- mt_cmd("iw", ["dev", card, "set", "channel", channel, width]),
          do: :ok
   end
 
@@ -32,7 +32,7 @@ defmodule NervesWfbNg do
   def set_card_tx_power(card, mode \\ "fixed", power_index) do
     with true <- power_index >= 0 and power_index <= 63,
          power_index = to_string(-power_index * 100),
-         {:ok, _} <- cmd("iw", ["dev", card, "set", "txpower", mode, power_index]),
+         {:ok, _} <- mt_cmd("iw", ["dev", card, "set", "txpower", mode, power_index]),
          do: :ok
   end
 
@@ -40,42 +40,49 @@ defmodule NervesWfbNg do
   Generates pair of keys(drone.key, gs.key) using wfb_keygen to be
   used on drone and groundstation side.
   """
-  def generate_wfb_keys, do: cmd(bundlex_path(:wfb_keygen))
+  def generate_wfb_keys, do: mt_cmd(bundlex_path(:wfb_keygen))
 
   @doc """
-  Starts wfb_tx or wfb_rx and creates StringIO device where output is stored
+  Starts wfb_tx and creates StringIO device where output is stored
   """
-  def start_wfb(card, mode, port, radio_id \\ 0, key \\ nil)
-
-  def start_wfb(card, :tx, port, radio_id, key) do
-    key_args = ["-K", key || "drone.key"]
-    port_args = ["-u", to_string(port)]
-    radio_id_args = ["-p", to_string(radio_id)]
-    args = Enum.concat([key_args, port_args, radio_id_args, [card]])
+  def start_wfb_tx(card, opts \\ []) do
+    defaults = [port: 5001, radio_id: 0, key: "drone.key", mcs_i: 0, bandwidth: 20]
+    args = defaults |> Keyword.merge(opts) |> Enum.flat_map(&prepare_args(&1)) |> Enum.concat([card])
 
     cmd_path = bundlex_path(:wfb_tx)
     {:ok, log_device_pid} = StringIO.open("")
     log_io_stream = IO.stream(log_device_pid, :line)
 
-    pid = spawn(fn -> cmd(cmd_path, args, into: log_io_stream) end)
+    pid = spawn(fn -> mt_cmd(cmd_path, args, into: log_io_stream) end)
 
     {pid, log_device_pid}
   end
 
-  def start_wfb(card, :rx, port, radio_id, key) do
-    key_args = ["-K", key || "gs.key"]
-    port_args = ["-u", to_string(port)]
-    radio_id_args = ["-p", to_string(radio_id)]
-    args = Enum.concat([key_args, port_args, radio_id_args, [card]])
+  @doc """
+  Starts wfb_rx and creates StringIO device where output is stored
+  """
+  def start_wfb_rx(card, opts \\ [])
+  def start_wfb_rx(card, opts) when is_binary(card), do: start_wfb_rx([card], opts)
+  def start_wfb_rx(cards, opts) do
+    defaults = [port: 5001, radio_id: 0, key: "gs.key", link_id: 7669206]
+    args = defaults |> Keyword.merge(opts) |> Enum.flat_map(&prepare_args(&1)) |> Enum.concat(cards)
 
     cmd_path = bundlex_path(:wfb_rx)
     {:ok, log_device_pid} = StringIO.open("")
     log_io_stream = IO.stream(log_device_pid, :line)
 
-    pid = spawn(fn -> cmd(cmd_path, args, into: log_io_stream) end)
+    pid = spawn(fn -> mt_cmd(cmd_path, args, into: log_io_stream) end)
 
     {pid, log_device_pid}
   end
+
+  defp prepare_args({:port, port}), do: ["-u", to_string(port)]
+  defp prepare_args({:key, key}), do: ["-K", key]
+  defp prepare_args({:radio_id, radio_id}), do: ["-p", to_string(radio_id)]
+  defp prepare_args({:link_id, link_id}), do: ["-i", to_string(link_id)]
+  defp prepare_args({:mcs_i, mcs_i}), do: ["-M", to_string(mcs_i)]
+  defp prepare_args({:bandwidth, bandwidth}), do: ["-B", to_string(bandwidth)]
+
 
   defp bundlex_path(native_name) do
     app = Application.get_application(__MODULE__)
@@ -83,10 +90,10 @@ defmodule NervesWfbNg do
     Bundlex.build_path(app, native_name, :port)
   end
 
-  defp cmd(cmd, args \\ [], options \\ []) do
+  defp mt_cmd(cmd, args \\ [], options \\ []) do
     case MuonTrap.cmd(cmd, args, options) do
       {output, 0} -> {:ok, output}
-      {output, err_code} -> {:error, "Error code: #{err_code} \n #{output}"}
+      {output, err_code} -> {:error, "Error code: #{err_code} \n #{inspect output}"}
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule NervesWifibroadcast.MixProject do
   defp deps do
     [
       {:bundlex, "~> 1.5"},
-      {:muontrap, "~> 1.5"}
+      {:muontrap, "~> 1.5"},
+      {:procket, "~> 0.9.9"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
